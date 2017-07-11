6903f02c66bb312dde0dc2e262ddd3243ae70cf7
Angular CLI <angular-cli@angular.io>
2017-07-11 15:16:21 +0900

    chore: initial commit from @angular/cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { HandsonPage } from './app.po';
+
+describe('handson App', () => {
+  let page: HandsonPage;
+
+  beforeEach(() => {
+    page = new HandsonPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!!');
+  });
+});
